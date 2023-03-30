5074d64308a9680397b5da40d9779e33966883ad
joepdonkers <joepie.donkers@gmail.com>
2023-03-30 11:22:53 +0200

    test?

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app/app.module';
+import { HttpStatus } from '@nestjs/common';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+describe('AuthController (e2e)', () => {
+	let app: INestApplication;
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	describe('/register', () => {
+		it('should return 201 status code with a valid payload', async () => {
+			const payload = {
+				name: 'John Doe',
+				email: 'johndoe@example.com',
+				password: 'p',
+				jobTitle: 'Developer',
+				role: 'user',
+				organisations: ['org1'],
+			};
+
+			const response = await request(app.getHttpServer())
+				.post('/register')
+				.send(payload)
+				.expect(HttpStatus.CREATED);
+
+			expect(response.body).toHaveProperty('id');
+			expect(typeof response.body.id).toBe('string');
+		});
+	});
 });
